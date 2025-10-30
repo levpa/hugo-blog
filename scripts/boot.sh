fe26c7d2eb62a18de81719cf08dc78e26e62e78b
Lev Pasichnyi <23581449+levpa@users.noreply.github.com>
2025-10-30 09:29:53 +0000

    fix: devcontainer image bump

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo apt-get update && sudo apt-get install -y dnsutils traceroute net-tools tree time
+sudo apt-get update
+sudo apt-get install -y dnsutils net-tools tree time
 
-echo "✅ DNS and net utils:"
-echo "$(dig -v && nslookup -version && host -V && mdig -v && traceroute -V && ifconfig -V)"
-echo "✅ system utils:"
-echo "$(tree --version)"
+echo "✅ DNS/network tools:"
+echo "$(dig -v && nslookup -version && host -V && mdig -v && ifconfig -V)"
+
+echo "✅ System tools:"
+echo "tree $(tree --version | awk '{print $2}')"
+echo "make $(make --version | head -n1 | awk '{print $3}')"
+echo "bash $(bash --version | head -n1 | awk '{print $4}')"
+
 
 echo "✅ $(git --version)"
-echo "✅ GitHub CLI: $(gh version)"
 echo "✅ $(yamllint --version)"
 echo "✅ markdownlint: $(markdownlint --version)"
-echo "✅ $(hugo version | awk '{print $1, $2}')"
-echo "✅ $(go version)"
-echo "✅ Node: $(node -v) NPM: $(npm -v)"
-echo "✅ Python: $(pip3 --version)"
+echo "✅ $(hugo version | awk '{split($2,a,"-"); print $1, a[1]}')"
+echo -e "✅ Node: $(node -v)\n✅ npm: $(npm -v)"
+echo "✅ Python: $(pip3 --version | awk '{print $2}')"
